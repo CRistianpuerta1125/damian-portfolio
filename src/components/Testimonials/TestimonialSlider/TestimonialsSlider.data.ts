01c7d4cf18e962decb4017c2796e605b7d15c506
Damian Puerta <55324669+CRistianpuerta1125@users.noreply.github.com>
2023-11-17 16:26:09 -0500

    changes and testimonials 4

--- src/components/Testimonials/TestimonialSlider/TestimonialsSlider.data.ts
+++ src/components/Testimonials/TestimonialSlider/TestimonialsSlider.data.ts
@@ -1,16 +1,16 @@
 export const testimonialsData = [
   {
     id: 0,
-    name: "Juan Palomo",
+    name: "Felipe Baquero",
     testimonial:
-      "Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate enim urna, nec fringilla mauris mattis a. In mollis vitae velit ac vehicula.",
+      "Es muy bueno en lo que hace todo sale a la perfeccion se arreglan los bugs que se puedan presentar en el camino de desarrollo final",
     image: "profile1.png",
   },
   {
     id: 1,
-    name: "Felipe Vaquero",
+    name: "Valentina Ramirez",
     testimonial:
-      "Es muy bueno en lo que hace todo sale a la perfeccion se arreglan los bugs que se puedan presentar en el camino de desarrollo final",
+      "Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate enim urna, nec fringilla mauris mattis a. In mollis vitae velit ac vehicula",
     image: "profile2.png",
   },
   {
